78bedd5d5dc72fc7fcbbc6ec5e7a8af684be1ab3
cepalle <epalle.cyrille@gmail.com>
2019-12-05 13:16:54 +0100

    AStar start

--- internal/idastar.go
+++ internal/idastar.go
@@ -1,9 +1,21 @@
 package internal
 
-func AStartWithScoreMax() {
+func AStart(r Rubik, scoring func(Rubik) uint32) []RubikMoves {
+	return AStartWithScoreMax(r, scoring, ^uint32(0))
+}
+
+func AStartWithScoreMax(r Rubik, scoring func(Rubik) uint32, scoreMax uint32) []RubikMoves {
 
 }
 
-func IdaStar() {
+func IdaStar(r Rubik, scoring func(Rubik) uint32) []RubikMoves {
+	var res []RubikMoves
+
+	for i := uint32(0); ; i += 10 {
+		res = AStartWithScoreMax(r, scoring, i)
+		if res != nil {
+			return res
+		}
+	}
 
 }
