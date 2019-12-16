be19b95977c1a757ed2ee497a48d365fd36717cc
cepalle <epalle.cyrille@gmail.com>
2019-12-16 16:17:46 +0100

    try other lib ia

--- cmd/rubik/learn2.go
+++ cmd/rubik/learn2.go
@@ -8,8 +8,8 @@ import (
 	"math/rand"
 	"os"
 	"github.com/cepalle/rubik/internal/solve"
-	"github.com/goml/gobrain"
-	"github.com/goml/gobrain/persist"
+	deep "github.com/patrikeh/go-deep"
+	"github.com/patrikeh/go-deep/training"
 )
 
 func main() {
@@ -36,21 +36,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	// fmt.Println(all)
-
 	rand.Seed(0)
 
-	ff := &gobrain.FeedForward{}
-
-	ff.Init(48, 48, learn.Bfs_depth+1)
+	// ---
 
 	patterns := learn.MakePatterns(learn.Equalize(all, learn.Bfs_depth), learn.Bfs_depth)
-	// fmt.Println(patterns)
 
-	ff.Train(patterns, 1000, 0.001, 0.001, true)
-	err := persist.Save("./ff.network", ff)
-	if err != nil {
-		log.Println("impossible to save network on file: ", err.Error())
-	}
-	// ff.Test(patterns)
+	n := deep.NewNeural(&deep.Config{
+		Inputs: 2,
+		Layout: []int{48, 48, 36, 24, 12, learn.Bfs_depth + 1},
+		/* Activation functions: Sigmoid, Tanh, ReLU, Linear */
+		Activation: deep.ActivationSigmoid,
+		/* Determines output layer activation & loss function:
+		ModeRegression: linear outputs with MSE loss
+		ModeMultiClass: softmax output with Cross Entropy loss
+		ModeMultiLabel: sigmoid output with Cross Entropy loss
+		ModeBinary: sigmoid output with binary CE loss */
+		Mode: deep.ModeMultiClass,
+		/* Weight initializers: {deep.NewNormal(μ, σ), deep.NewUniform(μ, σ)} */
+		Weight: deep.NewNormal(1.0, 0.0),
+		Bias:   true,
+	})
+
+	// params: learning rate, momentum, alpha decay, nesterov
+	optimizer := training.NewSGD(0.05, 0.1, 1e-6, true)
+	// params: optimizer, verbosity (print stats at every 50th iteration)
+	trainer := training.NewTrainer(optimizer, 50)
+
+	training, heldout := data.Split(0.5)
+	trainer.Train(n, training, heldout, 1000) // training, validation, iterations
+
 }
