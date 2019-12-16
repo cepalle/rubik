4235cf33040daac6967b03f7784abfee8a682968
cepalle <epalle.cyrille@gmail.com>
2019-12-16 15:56:10 +0100

    split

--- cmd/rubik/bfs_gen.go
+++ cmd/rubik/bfs_gen.go
@@ -3,16 +3,16 @@ package main
 import (
 	"encoding/gob"
 	"fmt"
+	"github.com/cepalle/rubik/internal/learn"
 	"os"
 	"github.com/cepalle/rubik/internal/solve"
 )
 
 func main() {
-	const bfs_depth = 3
 
 	var all []solve.NodeExp
 
-	all = solve.Bfs_explorer(bfs_depth)
+	all = solve.Bfs_explorer(learn.Bfs_depth)
 
 	dataFile, err := os.Create("node_exp.gob")
 
---
+++ internal/learn/learn.go
@@ -0,0 +1,59 @@
+package learn
+
+import (
+	"github.com/cepalle/rubik/internal/makemove"
+	"github.com/cepalle/rubik/internal/solve"
+)
+
+const Bfs_depth = 3
+
+func MakePatterns(bfsRes []solve.NodeExp, bfs_depth uint32) [][][]float64 {
+	var patterns [][][]float64
+
+	for _, e := range bfsRes {
+		var input []float64
+		var output []float64
+
+		input = makemove.Rubik_to_nn_input(&e.Cube)
+		for i := uint32(0); i <= bfs_depth; i++ {
+			output = append(output, 0)
+		}
+		output[e.Depth] = 1
+
+		line := [][]float64{input, output}
+		patterns = append(patterns, line)
+	}
+	return patterns
+}
+
+func Equalize(bfsRes []solve.NodeExp, bfs_depth uint32) []solve.NodeExp {
+	var nbByDepth []uint32
+
+	for i := uint32(0); i <= bfs_depth; i++ {
+		nbByDepth = append(nbByDepth, 0)
+	}
+	for _, e := range bfsRes {
+		nbByDepth[e.Depth]++
+	}
+	var mx uint32
+	mx = 0
+
+	for _, e := range nbByDepth {
+		if e > mx {
+			mx = e
+		}
+	}
+
+	for i := uint32(0); i < uint32(len(nbByDepth)); i++ {
+		for ; nbByDepth[i] < mx; {
+			for j := int(0); j < len(bfsRes) && nbByDepth[i] < mx; j++ {
+				if bfsRes[j].Depth == i {
+					nbByDepth[i]++
+					bfsRes = append(bfsRes, bfsRes[j])
+				}
+			}
+		}
+
+	}
+	return bfsRes
+}
